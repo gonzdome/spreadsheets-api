8c3d09cb6757251ef71f3b8e529abaf55000a14a
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2023-10-09 22:22:35 -0300

    fix hard coded file extensions and file path

--- src/app/services/csv/CreateCSVFileService.js
+++ src/app/services/csv/CreateCSVFileService.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
+const { CSV } = require('../../../utils/SpreadsheetsTypes');
 
 module.exports = async (data) => {
+  const { FILE_DIR } = process.env;
+
   const newDateToIso = new Date().toISOString().split('T')[0];
   const randomNumberByDateNow = Math.floor(Date.now() / 1000);
 
-  const fileDir = './temp/';
-  const fileName = `${newDateToIso}${randomNumberByDateNow}.csv`;
-  const filePath = `${fileDir}${fileName}`;
+  const fileName = `${newDateToIso}${randomNumberByDateNow}.${CSV}`;
+  const filePath = `${FILE_DIR}${fileName}`;
 
   const csvHeaders = Object.keys(data[0]).join(',');
   const csvValues = data.map((row) => Object.values(row).join(',')).join('\n');
   const csvContent = `${csvHeaders}\n${csvValues}`;
 
   // Create path if it doesn't exist then create csv file
-  if (!fs.existsSync(fileDir)) fs.mkdirSync(fileDir, { recursive: true });
+  if (!fs.existsSync(FILE_DIR)) fs.mkdirSync(FILE_DIR, { recursive: true });
   fs.writeFileSync(filePath, csvContent, 'utf-8');
 
   return { fileName, filePath };
--- src/app/services/xlsx/CreateXLSXFileService.js
+++ src/app/services/xlsx/CreateXLSXFileService.js
@@ -1,23 +1,24 @@
 const fs = require('fs');
-const XLSX = require('xlsx');
+const { writeFile, utils } = require('xlsx');
+const { XLSX } = require('../../../utils/SpreadsheetsTypes');
 
 module.exports = async (data) => {
-  const { json_to_sheet: jsonToSheet, book_new: bookNew, book_append_sheet: bookAppendSheet } = XLSX.utils;
+  const { FILE_DIR } = process.env;
+  const { json_to_sheet: jsonToSheet, book_new: bookNew, book_append_sheet: bookAppendSheet } = utils;
 
   const newDateToIso = new Date().toISOString().split('T')[0];
   const randomNumberByDateNow = Math.floor(Date.now() / 1000);
 
-  const fileDir = './temp/';
-  const fileName = `${newDateToIso}${randomNumberByDateNow}.xlsx`;
-  const filePath = `${fileDir}${fileName}`;
+  const fileName = `${newDateToIso}${randomNumberByDateNow}.${XLSX}`;
+  const filePath = `${FILE_DIR}${fileName}`;
 
   const workSheet = jsonToSheet(data);
   const workBook = bookNew();
   bookAppendSheet(workBook, workSheet, `${newDateToIso}${randomNumberByDateNow}`);
 
   // Create path if it doesn't exist then create xlsx file
-  if (!fs.existsSync(fileDir)) fs.mkdirSync(fileDir, { recursive: true });
-  XLSX.writeFile(workBook, filePath);
+  if (!fs.existsSync(FILE_DIR)) fs.mkdirSync(FILE_DIR, { recursive: true });
+  writeFile(workBook, filePath);
 
   return { fileName, filePath };
 };
