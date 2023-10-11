e80693a851c950b9230f8b7b600dd2aa789d7803
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2023-10-11 17:46:06 -0300

    fix validator and model fields to snake case, new eslint rule

--- src/app/validator/CreateSpreadsheetValidator.js
+++ src/app/validator/CreateSpreadsheetValidator.js
@@ -4,5 +4,5 @@ module.exports = async (params) => {
   const { types, data } = params;
 
   if (!types.includes('xlsx') && !types.includes('csv')) throw HandleErrorHelper('Invalid Param', 'Choose the right types ex: "xlsx" and/or "csv"!');
-  if (data?.length === 0) throw HandleErrorHelper('Missing Param', 'Add the desired fields using JSON format, ex: { document: "...", name: "..."}');
+  if (!data || data?.length === 0) throw HandleErrorHelper('Missing Param', 'Add the desired fields using this format, ex: data: [{ document: "...", name: "..." }, ...]');
 };
