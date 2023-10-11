2f403214c9f0d98b345b3e08cd2402235bb5b302
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2023-10-11 16:58:28 -0300

    fix validator/model, add new eslint rule and helper to handle date

--- src/app/models/SpreadsheetModel.js
+++ src/app/models/SpreadsheetModel.js
@@ -1,15 +1,17 @@
 const { Schema, model } = require('mongoose');
+const { removeHours } = require('../helpers/HandleDateHelper');
 
-const SpreadsheetSchema = new Schema({
-  file_type: {
-    xlsx: Boolean,
-    csv: Boolean
-  },
-  file: {
-    xlsx: { fileName: String, filePath: String },
-    csv: { fileName: String, filePath: String }
-  },
-  data: { type: Schema.Types.Mixed }
-});
+const SpreadsheetSchema = new Schema(
+  {
+    file_types: { type: Array },
+    file: {
+      xlsx: { fileName: String, filePath: String },
+      csv: { fileName: String, filePath: String }
+    },
+    data: { type: Array },
+    created_at: { type: Date, default: removeHours(new Date(), 3) },
+    updated_at: { type: Date, default: removeHours(new Date(), 3) }
+  }
+);
 
 module.exports = model('Spreadsheet', SpreadsheetSchema);
